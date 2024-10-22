42724b23799d02ffd19e588e35151d90059a9853
Prabhu Kalyan Korivi <prabhukalyan30@gmail.com>
2024-10-22 23:51:36 +0530

    commit files

---
+++ src/main/java/com/prabhu/thymeleafspringsecurity/ThymeleafspringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.prabhu.thymeleafspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafspringsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prabhu/thymeleafspringsecurity/ThymeleafspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhu.thymeleafspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafspringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
